2ff646859658c73a03505f1a8a4d64ff11be53a0
Adrian Makridenko <maccree.a@yandex.ru>
2024-04-14 16:07:34 +0300

    Delete unused endpoint in drf

--- django-template-drf/config/urls.py
+++ django-template-drf/config/urls.py
@@ -10,9 +10,3 @@ urlpatterns = [
     path('admin/', admin.site.urls),
 ]
 
-urlpatterns += [
-    re_path(r'', 
-        ensure_csrf_cookie(TemplateView.as_view(template_name='index.html')), 
-        name='index',
-    )
-]
