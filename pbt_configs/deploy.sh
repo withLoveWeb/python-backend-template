5280e93adda78c41b8ae6749c3dda44df89406f9
BOBA <withLoveWeb7@gmail.com>
2024-10-02 21:38:11 +0300

    fix: deploy

--- pbt_configs/deploy.sh
+++ pbt_configs/deploy.sh
@@ -4,10 +4,15 @@ PROJECT=${PWD##*/}
 
 if [[ $1 = 'local' ]]; then
 
-    cat ./server/.env.example >> ./kernel/.env
+    cp ./server/.env.example >> ./kernel/.env
     echo "Don't forget to change your SECRET_KEY"
     echo "https://djecrety.ir/ can help you!"
-    cat ./server/config/local_settings.example >> ./server/config/local_settings.py
+    cp ./server/config/local_settings.example >> ./server/config/local_settings.py
+
+    exit 0
+fi
+
+if [[ $1 = 'prod' ]]; then
 
     exit 0
 fi
